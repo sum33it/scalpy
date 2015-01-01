825689d94c561b4b0215691a56c75c972ab26547
sum33it <sumit.phy33@gmail.com>
2015-01-01 16:48:17 +0530

    Create __init__.py

---
+++ scalpy/__init__.py
@@ -0,0 +1,11 @@
+
+__all__ = ['scalar','solver','fluids','transfer_func']
+
+from .scalar import *
+from .solver import *
+from .fluids import *
+from .transfer_func import *
+
+__version__ = "0.1.0a"
+__author__ = "Sumit Kumar, Abhishek Jana and Anjan A. Sen"
+__license__ = "GNU GENERAL PUBLIC LICENSE v3"
